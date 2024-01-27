c2bfa6166330c9ced19e20e4219e6fa6ee92475c
Andhikap <prameswaara@gmail.com>
2024-01-27 15:06:45 +0700

    learn variable expression

--- src/main/java/com/prameswaradev/learnthymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/prameswaradev/learnthymeleaf/controller/HelloWorldController.java
@@ -1,10 +1,13 @@
 package com.prameswaradev.learnthymeleaf.controller;
 
 
+import com.prameswaradev.learnthymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/")
@@ -13,6 +16,7 @@ public class HelloWorldController {
     @GetMapping
     public String getHello(Model model){
         model.addAttribute("msg", "Hello World!");
+        model.addAttribute("user", new User(UUID.randomUUID(), "Andhika", "Jakarta"));
         return "hello";
     }
 }
